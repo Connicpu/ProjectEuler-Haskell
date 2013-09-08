602536eb8b7e2ec83f0167c0b04a6bfd4b6561ec
Connor Hilarides <connorcpu@live.com>
2013-09-08 02:39:15 -0700

    Problem 10

--- 010.hs
+++ 010.hs
@@ -1,2 +1,4 @@
-import ConnorCommon
+import ConnorCommon (primesTo)
 
+main :: IO ()
+main = putStrLn $ "Sum of all primes below 2,000,000: " ++ (show $ sum (primesTo 2000000))
--- connorcommon.hs
+++ connorcommon.hs
@@ -1,13 +1,32 @@
-module ConnorCommon (isPrime, intSquareRoot, isFactor) where
+module ConnorCommon (isPrime, intSquareRoot, isFactor, primes, primesTo) where
+  import Data.List
+  import qualified Data.Map as M
+
   isFactor :: Integer -> Integer -> Bool
-  isFactor x y = mod y x == 0
+  isFactor x y = y `mod` x == 0
 
   intSquareRoot :: Integer -> Integer
   intSquareRoot x = (floor . sqrt . fromIntegral) x
 
   isPrime :: Integer -> Bool
-  isPrime x = primeCheck x (intSquareRoot x)
+  isPrime x = length ([ y | y <- [2..mid], isFactor y x ]) == 0
+    where mid = intSquareRoot x
 
-  primeCheck :: Integer -> Integer -> Bool
-  primeCheck x 1 = True
-  primeCheck x y = not (isFactor y x) && (primeCheck x (y - 1))
+  lessThanFilter x y = (x > y)
+  primes = primesMPE
+  primesTo n = takeWhile (lessThanFilter n) primes
+   
+  primesMPE :: [Integer]
+  primesMPE = 2:mkPrimes 3 M.empty prs 9   -- postponed addition of primes into map;
+    where                                  -- decoupled primes loop feed 
+      prs = 3:mkPrimes 5 M.empty prs 9
+      mkPrimes n m ps@ ~(p:t) q = case (M.null m, M.findMin m) of
+          (False, (n', skips)) | n == n' ->
+               mkPrimes (n+2) (addSkips n (M.deleteMin m) skips) ps q
+          _ -> if n<q
+               then    n : mkPrimes (n+2)  m                  ps q
+               else        mkPrimes (n+2) (addSkip n m (2*p)) t (head t^2)
+   
+      addSkip n m s = M.alter (Just . maybe [s] (s:)) (n+s) m
+      addSkips = foldl' . addSkip
+
